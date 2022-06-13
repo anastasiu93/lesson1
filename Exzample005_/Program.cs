e812276b944110cdbf934a63e78e6363da77047c
Anastasiu <anastastasiu.9315@gmail.com>
2022-06-13 23:23:01 +1100

    0003

--- Exzample005_/Program.cs
+++ Exzample005_/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Ввод имени (выделяем определенное имя)
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
